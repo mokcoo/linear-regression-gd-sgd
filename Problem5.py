c4d2bae9964546f3c9c19ff3281759b934d1cc38
mrigankdoshy <mrigankdoshy@gmail.com>
2021-02-19 10:46:59 -0500

    Init

---
+++ Problem5.py
@@ -0,0 +1,11 @@
+import math
+import random
+import numpy as np
+
+
+def bgd_l2(data, y, w, eta, delta, lam, num_iter):
+    return new_w, history_fw
+
+
+def sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
+    return new_w, history_fw
---
+++ Problem5Plot.py
@@ -0,0 +1,7 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # Put the code for the plots here, you can use different functions for each
+    # part
