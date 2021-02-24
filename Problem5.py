9acc5aa37ff11b147b0e535372bd61b4f2a05183
mrigankdoshy <mrigankdoshy@gmail.com>
2021-02-24 13:31:21 -0500

    bgd_l2 + regularization

--- Problem5.py
+++ Problem5.py
@@ -16,7 +16,36 @@ def objective_function(data, y, w, delta):
     return f / 100
 
 
+def regularization(w, lam):
+    return lam * ((w[0] ** 2) + (w[1] ** 2))
+
+
 def bgd_l2(data, y, w, eta, delta, lam, num_iter):
+    history_fw = []
+    for j in range(0, num_iter):
+        w0_gradient = 0
+        w1_gradient = 0
+
+        for i in range(0, 100):
+            wx = (w[0] * data[i][0]) + (w[1] * data[i][1])
+            if y[i] >= wx + delta:
+                w0_gradient += -(2/100) * data[i][0] * (y[i] - wx - delta)
+                w1_gradient += -(2/100) * (y[i] - wx - delta)
+            elif abs(y[i] - wx) < delta:
+                w0_gradient += 0
+                w1_gradient += 0
+            elif y[i] <= wx - delta:
+                w0_gradient += -(2/100) * data[i][0] * (y[i] - wx + delta)
+                w1_gradient += -(2/100) * (y[i] - wx + delta)
+
+        w0_gradient += 2 * lam * w[0]
+        w1_gradient += 2 * lam * w[1]
+        w[0] -= (eta * w0_gradient)
+        w[1] -= (eta * w1_gradient)
+
+        history_fw.append(objective_function(data, y, w, delta)
+                          + regularization(w, lam))
+    new_w = [w[0], w[1]]
     return new_w, history_fw
 
 
